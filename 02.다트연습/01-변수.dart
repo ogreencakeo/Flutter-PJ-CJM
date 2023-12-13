335f63d805636a4e90076c2c3d9d5b67f3ad9947
ogreencakeo <magente2000@gmail.com>
2023-12-13 17:13:18 +0900

    sdf

--- 02.다트연습/01-변수.dart
+++ 02.다트연습/01-변수.dart
@@ -1,4 +1,7 @@
 void main(List<String> args) {
+
+  // 다트언어는 변수형을 자동으로 인식하는 선언이 있음
+  // 1. var 
   var myHome = "내집이야~!";
   print(myHome);
   // var는 처음에 할당한 값의 형으로 재할당해야함!
@@ -6,6 +9,15 @@ void main(List<String> args) {
   myHome = "너의집도 크다!";
   print(myHome);
 
+  // 2. dynamic 
+  // -> var와 비슷하지만 나중에 재할당시 
+  // 처음 할당한 값과 다른 형의 값을 넣어도 괜찮다.
+  dynamic myId = 'hhh2345';
+  print('나의 아이디는 $myId');
+  myId = 787878;
+  print('너의 아이디는 $myId');
+
+  // [ 문자형 출력방법 : '${코드}' 또는 '${변수}' ]
 
   // 다트언어는 변수의 형을 선언함이 기본이다!
   // 1. 숫자형 : 
@@ -28,11 +40,11 @@ void main(List<String> args) {
   // 2. 문자형 : String
   String name = "톰 행크스";
   // name = 88;
-  print("나는 "+name+"입니다!");
+  print("나는 $name 입니다!");
 
   // 3. 불린형 : bool
   bool isTrue = true;
-  print(isTrue);
+  print("진짜인가? 가짜인가? $isTrue");
 
   // 4. 콜렉션 : 묶음데이터
   // List / Set / Map
@@ -41,11 +53,28 @@ void main(List<String> args) {
   print(we[2]+'는 모두 친구입니다!');
   print(we.length);
 
-  // 4-2. Set형 - 순서가 없고 중복되지 않는 데이터집합
-  Set<int> evens = {2,4,6,8,10};
+  // 4-2. Set형 - 
+  // -> 순서가 없고 중복되지 않는 데이터집합
+
+  // Set<int> evens = {2,4,6,8,10};
+  // Set<dynamic> evens = {2,4,6,8,10,4,'짝수'};
+  var evens = {2,4,6,8,10,4,'짝수'};
   print(evens);
 
   // 4-3. Map형 - 데이터에 라벨링된 데이터 집합(객체와 유사)
   Map<String,String> actor = {"이름":"강동원","나이":"40"};
   print(actor);
+
+  // [ 다트의 형변환 (Casting) ]
+  // -> 특정 데이터 형에서 다른 형으로 변환하는 것을
+  // 흔히 캐스팅이라고도 함!
+  // -> 일반적인 형변환 : 변수형.parse(대상변수)
+  // 주의 : 형변환이 안될 것 같으면 변환하지 마라!
+  // -> 예컨데 문자형 문자를 숫자로 변경못함!
+  String stNum = '777';
+  print('문자형 숫자: $stNum');
+
+  // 문자형숫자를 캐스팅하여 숫자로 계산하기
+  num result = 111 + int.parse(stNum);
+  print('111 + 777 = $result');
 }
