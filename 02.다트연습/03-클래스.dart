8d0c4df700734e233bb7310c2d2c3e94148fc3ec
ogreencakeo <magente2000@gmail.com>
2023-12-20 11:19:59 +0900

    Update 03-클래스.dart

--- 02.다트연습/03-클래스.dart
+++ 02.다트연습/03-클래스.dart
@@ -1,6 +1,49 @@
 void main(List<String> args) {
+  // Dog 클래스 메모리에 생성하기
+  // 클래스를 담는 변수의 형은 그 클래스명을 사용한다!
+  Dog d1 = Dog();
+  print('나의 강아지 종류 이름은 ${d1.name}이다.');
   
-}
+  // 클래스의 속성값 업데이트하기 
+  d1.name = '닥스훈트';
+  print('1년후 나의 새로운 강아지 종류명은 ${d1.name}이다.');
+  // 중요한 것은 클래스의 속성자체의 정의다!
+  // 값은 그것을 사용하는 사용자의 등록에 따라 달라질 수 있다!
+  
+  print('내 강아지의 색깔은 ${d1.color}이고 나이는 ${d1.age}살이다!');
+  
+  print('훈련1회 실시함! 물기지수 ${d1.traningDog()}%');
+
+  dynamic doit;
+  for(var i=0; i<10; i++){
+    doit = d1.traningDog();
+  }
+
+  print('훈련 10회 실시함! 물기지수는 $doit%');
+
+  // 다른 변수에 Dog 클래스를 생성하여 사용!
+  // 기존 d1과 연결성은 없다!
+  Dog d2 = Dog();
+  print('나의 사랑하는 강아지의 종류는 ${d2.name}이다!');
+
+  // 값을 생성시에 초기화 하는 Cat 사용하기 //////////
+  Cat c1 = Cat("코리안숏헤어", 4, "갈색얼룩");
+  print('나의 고양이 종은 ${c1.name}이고 나이는 ${c1.age}살, 색깔은 ${c1.color}이다!');
+
+  // c1.name = '이집트 고양이'; -> final이면 재할당 불가!
+  print('나의 고양이종을 다시 말하면 ${c1.name}이다!');
+
+  // 상수 테스트 : const, final
+  const aa = 'aa';
+  // const bb; -> 선언과 동시에 할당해야함!
+  final bb; // final은 선언후 처음 할당되는 것을 상수화함
+  bb = 'bb';
+  // bb = 'bbb'; -> 재할당은 불가함!
+  print('${aa} ${bb}');
+  // 선언과 할당 분리 특성에 따라 클래스에서 주로 사용됨
+  // -> 클래스에서 const 사용불가!
+
+} // main ////////
 
 /****************************************************************
   [ 다트의 클래스 ]
@@ -13,9 +56,46 @@ void main(List<String> args) {
 
   형식 : class 키워드를 사용하여 파스칼 케이스로 명명함.
   class MyClassIsPerfect{
-    코드...
+    변수선언 및 할당
+    생성자 메서드(){}
+    메서드()
   }
+  -> 변수는 선언만 할 수도 있고 할당까지 할 수 있음
+  -> 생성자메서드는 클래스를 사용시 가장 먼저 호출되는 메서드
+  -> 개별적인 클래스 구성 메서드를 생성할 수 있음
+
 ****************************************************************/
+
+// 애완동물 클래스 : 부모클래스
+class Pet{
+  // 애완동물 종류
+  final String species;
+  // 성격
+  final String character;
+  // 먹이종류
+  final String food;
+
+  Pet(this.species, this.character, this.food){
+    print('부모 Pet 클래스 생성자!');
+  }
+
+  String hearSound(String sp){
+    dynamic retVal;
+    switch(sp){
+      case '고양이' :
+        retVal = '야옹';
+      break;
+      case '강아지' :
+        retVal = '멍멍';
+      break;
+      default:
+        retVal = '동물소리';
+    } // switch case ////
+    // 리턴코드
+    return retVal;
+  } // hearSound 메서드 ///////
+
+} // Pet 클래스 /////
 
 // 멍멍이 클래스
 class Dog{
@@ -23,24 +103,48 @@ class Dog{
   String name = '시바견';
   int age = 8;
   String color = '검정색';
+  // 물기지수
   int bite = 100;
 
   // 클래스 메서드
-  void traningDog(){
+  int traningDog(){
     bite = bite - 5;
+    
+    // 5% ~ 100% 값 한계설정
+    if(bite<5) bite = 5;
+    else if(bite>100) bite = 100;
+    
+    // 리턴값
+    return bite;
   }
 } // Dog 클래스 /////
 
 // 야옹잉 클래스
 class Cat{
   // 클래스 속성들
-  String name = '먼치킨';
-  int age = 5;
-  String color = '얼룩색';
+  // 고양이 이름
+  String name;
+  // 고양이 년수
+  final int age;
+  // 고양이 색깔
+  final String color;
   int punch = 100;
+
+  // 속성들의 값 초기화는 코드에서 해준다
+  // 이것을 해주도록 설정하는 메서드는 무조건 실행되는
+  // 클래스의 필수 메서드인 생성자 메서드에 해준다!
+  // 생성자 메서드는 클래스와 이름이 같음
+  // this 키워드 : 클래스 내부 멤버(속성)을 지칭함!
+  // 생성자메서드(){} -> 소괄호안에 콤마로 초기값 셋팅함
+  // 중괄호를 생략할 수가 있음 (구현코드가 없다면 .... )
+  // -> 생성자메서드();
+  Cat(this.name, this.age, this.color){
+    print('Cat 생성자 함수 코드구역');
+  }
 
   // 클래스 메서드
   void traningCat(){
     punch = punch - 5;
   }
+
 }
