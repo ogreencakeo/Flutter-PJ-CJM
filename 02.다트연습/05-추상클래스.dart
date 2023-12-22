f1deab31c901fcb347fe880bf12a895df6c304ac
ogreencakeo <magente2000@gmail.com>
2023-12-22 12:16:54 +0900

    sdf

--- 02.다트연습/05-추상클래스.dart
+++ 02.다트연습/05-추상클래스.dart
@@ -1,11 +1,28 @@
-// [ 다트 추상클래스 : feat.인터페이스 ]
-void main(List<String> args) {
-  
-} ///////////// main //////////////////
+/****************************************** 
+ [다트 추상클래스]
+ -> 형식을 강제할때 사용하는 클래스 생성방법
+ -> 일반 상속과 달리 추상클래스는 모든 속성과
+ 메서드를 다 구현해야함!
+ * 사용법:
+ * abstract 키워드로 클래스를 선언한다->추상클래스
+ * abstract는 추상화한...라는 뜻
+ * 
+  abstract class 추상클래스명{
+    구현부
+  }
+ 
+ * 추상클래스를 상속 받는 것처럼 연결하는 방법은
+ * implements 키워드로 연결한다!
+ * implement는 시행하다...라는 뜻!
+ * -> 추상클래스에 정의된 모든 속성과 메서드를 구현해야함!
+ 
+ class 클래스명 implements 추상클래스명 {
+    구현부
+ }
+******************************************/
 
-
-// 추상 클래스 연습 : 사람 클래스를 추상클래스로 만들기
-// -> 특정인물 클래스에서 사람클래스를 받아서 구현함
+// 추상클래스 연습 : 사람클래스를 추상클래스로 만들기
+// -> 특정인물 클래스에서 사람클래스를 받아서 구현함!
 
 abstract class Saram{
   // 생성자
@@ -35,7 +52,11 @@ abstract class Saram{
 // 도경수 배우의 클래스
 class KyungSu implements Saram{
   @override
-  String get name => '도경수';
+  late String _name; // 셋팅변수
+  String get name => _name;
+  set name(String name) => _name = name;
+
+  // String get name => '도경수';
   // get 키워드는 겟터를 사용하는 변수를 선언함
 
   @override
@@ -45,7 +66,8 @@ class KyungSu implements Saram{
   String age() => '$name씨의 나이는 30살입니다!';
 
   @override
-  String eat() => '$name씨는 피자를 먹는다!';
+  // String eat() => '$name씨는 피자를 먹는다!';
+  String eat(){return '$name씨는 피자를 먹는다!';}
 
   @override
   String say() => '$name씨는 무엇인가 말하고 있다!';
@@ -63,3 +85,105 @@ class KyungSu implements Saram{
  * - 오버로드는 같은 이름의 메서드의 파라미터만 그 개수가 
  * 다르게 여러개 만들 수 있는 것을 말함.
 *****************************************************************************/
+/*************************************** 
+  [ 겟터 / 셋터 ]
+  변수값 읽기와 할당을 하도록 캡슐화를 하여
+  값 셋팅시 정보를 은닉하여 변수를 관리하는 방법
+
+  ((방법:))
+  late 변수형 셋팅변수명;
+  변수형 get 변수명 => 셋팅변수명;
+  set 변수명(변수형 변수명) -> 셋팅변수명 = 변수명
+
+  ((겟터/셋터 변수명))
+  일반적으로 변수명과 셋팅변수명은 같은이름이며 
+  셋팅변수명 앞에 언더바(_)를 붙여구분함
+  예) 변수명-> name, 셋팅변수명->_name
+
+  ((late 키워드))
+  선언한 변수가 바로 할당되지 못할 경우
+  late 키워드를 앞에 사용하여 사용직전에
+  할당될 것임일 알려주면 에러를 막을 수 있다!
+
+  ((겟터/셋터 값할당))
+  -> 외부에서 인스턴스 생성후 셋터를 통해 
+  값을 셋팅할 경우 변수명에 할당하면 된다!
+  클래스.변수명 = 값
+***************************************/
+
+// 남지현 배우 클래스 : Saram 클래스 일반 상속하기
+// 일반 상속은 부모클래스의 모든 내용을 재정의 할 필요가 없음!
+// - 단, 구체적인 정의가 없는 메서드는 필수 재정의!!
+class JeeHyun extends Saram{
+  // 생성자 : 받은 값을 부모클래스로 넘겨 먼저 셋팅함!
+  JeeHyun(String name) : super(name);
+  @override
+  String gender() => '$name씨는 여자다!';
+  @override
+  String age() => '$name씨는 28살이다!';
+} // JeeHyun 클래스 /////////
+
+// 일반적인 남자를 나타내는 클래스 /////////
+// 추상클래스임! -> 추상클래스를 일반상속한 추상클래스임!
+abstract class Namja extends Saram{
+  // 생성자
+  Namja(String name) : super(name);
+
+  // 필수 재정의는 없고 원하는 것을 재정의함!
+  @override
+  String gender() => '$name씨는 남자다!';
+} // Namja 추상 클래스 //////////
+
+// 일반적인 여자를 나타내는 클래스 /////////
+// 추상클래스임! -> 추상클래스를 일반상속한 추상클래스임!
+abstract class Yeoja extends Saram{
+  // 생성자
+  Yeoja(String name) : super(name);
+
+  // 필수 재정의는 없고 원하는 것을 재정의함!
+  @override
+  String gender() => '$name씨는 여자다!';
+} // Yeoja 추상 클래스 //////////
+
+// 박서준 배우의 클래스 //////////////////
+// 남자 추상클래스를 상속받는다
+// 추상클래스를 일반 상속한 클래스는 추상클래스의 미정의 메서드를 
+// 필수 정의 해야하는데 gender() 메서드는 상속받은 
+// 추상클래스인 Namja에서 이미 재정의 했으므로 
+// Saram 추상클래스에 남은 재정의 메서드 항목은 age만 남는다.
+class SeoJun extends Namja{
+  SeoJun(String name) : super(name);
+
+  // 필수 재정의 항목 : age()
+  String age() => '$name씨는 39살이다.';
+
+  // 내가 추가로 하고 싶은 재정의하기
+  String eat() => '$name씨는 햄버거를 먹는다!';
+} // SeoJun 클래스 //////////////
+
+// 테스트를 수행하는 함수
+void testClass(){
+  // 추상클래스를 수현한 경수클래스 인스턴스생성
+  KyungSu ks = KyungSu();
+  ks.name = '도경수';
+  print(ks.eat());
+  print(ks.say());
+  print(ks.gender());
+  // print(KyungSu().eat());
+  // print(KyungSu().say());
+
+  // 추상클래스를 일반 상속받은 지현클래스 인스턴스 생성
+  JeeHyun jh = JeeHyun('남지현');
+  print(jh.age());
+  print(jh.gender());
+
+  // 추상클래스의 상속한 추상클래스를 상속한 서준 클래스 인스턴스 생성
+  SeoJun sj = SeoJun('박서준');
+  print(sj.age());
+  print(sj.eat());
+} // testClass
+
+main(){
+  // 테스트 함수 호출
+  testClass();
+} // main
