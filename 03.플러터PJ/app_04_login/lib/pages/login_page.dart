40cac87f5f9eaca74a4ee0e25e4842a1e53d40e2
ogreencakeo <magente2000@gmail.com>
2024-01-10 12:55:32 +0900

    sdf

--- 03.플러터PJ/app_04_login/lib/pages/login_page.dart
+++ 03.플러터PJ/app_04_login/lib/pages/login_page.dart
@@ -1,5 +1,7 @@
 // 로그인 페이지 /////////
 import 'package:flutter/material.dart';
+//로고 위젯 추가
+import 'package:app_04_login/components/logo.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -7,8 +9,25 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.red,
+      // 리스트뷰를 써야만 스크롤을 사용하여
+      // 하단 입력 키보드가 올라올때 대비할 수 있다!
+      body: ListView(
+        children: [
+          SizedBox(height: 20),
+          // 로고보이기
+          Logo('Login'),
+          // 임시 이동 버튼
+          TextButton(
+            // 대상을 누를때 발생
+            onPressed: () {
+              // debugPrint('나눌렸어?');
+              // 라우터 이동은 Navigator
+              // pushNamed() 메서드로!!!
+              Navigator.pushNamed(context, '/home');
+            },
+            child: Text('Go to Home!'),
+          )
+        ],
       ),
     );
   }
