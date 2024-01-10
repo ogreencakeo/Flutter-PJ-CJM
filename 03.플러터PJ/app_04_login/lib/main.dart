8ce859425006d4a6e4c26642e1e139786b7907f4
ogreencakeo <magente2000@gmail.com>
2024-01-10 12:06:03 +0900

    dfg

--- 03.플러터PJ/app_04_login/lib/main.dart
+++ 03.플러터PJ/app_04_login/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+// 홈페이지
+import 'package:app_04_login/pages/home_page.dart';
+// 로그인 페이지
+import 'package:app_04_login/pages/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,11 +10,12 @@ void main() {
 
 /*
   [ 로그인 앱 전체 구성 : lib하위 ]
-  - 메인 출발 ( 추가 사이즈 지정 size.dart )
-  - 페이지 구성 : 2개
+  1. 메인 출발 ( 추가 사이즈 지정 size.dart )
+  -> main.dart에 라우터 속성 추가
+  2. 페이지 구성 : 2개
     pages / home_page.dart : 홈페이지
     pages / login_page.dart : 로그인 페이지
-  - 컴포넌트 구성 : 3개
+  3. 컴포넌트 구성 : 3개
   components / logo.dart : SVG로고
   components / custom_form.dart : Form 위젯
   components / custom_text_form_field.dart : Form요소 위젯
@@ -24,6 +29,38 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+
+      // [ 테마 설정 : 공통버튼 테마 ]
+      theme: ThemeData(
+        // 텍스트 버튼 테마 설정하기
+        textButtonTheme: TextButtonThemeData(
+          // 버튼 스타일 속성 지정
+          style: TextButton.styleFrom(
+              // 배경색
+              backgroundColor: Colors.black,
+              // 글자색
+              foregroundColor: Colors.white,
+              // 그림자효과 -> 둥근 모서리
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30)),
+              // 버튼 최소 크기 설정
+              minimumSize: Size(400, 60)),
+        ),
+      ),
+
+      // [ 라우터 속성 셋업하기 ]
+      // 1. 첫페이지 라우터 설정 (initialRoute 속성)
+      initialRoute: '/login',
+      // 2. 기본 셋팅법
+      // routes : {'/라우터주소' : (context) => 클래스 생성자()}
+      routes: {
+        '/home': (context) => HomePage(),
+        '/login': (context) => LoginPage()
+      },
+      // 3. 라우터 호출 이동법 : 서로 다른 페이지에서 호출
+      // Navigator.pushNamed(context, '/라우터주소')
+
+      // 라우터를 사용할 셩우 home 속성을 사용하지 않는다!
       // home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
