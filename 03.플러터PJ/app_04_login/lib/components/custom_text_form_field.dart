3928edf58761757eddd3913221c05b647f8406fa
ogreencakeo <magente2000@gmail.com>
2024-01-11 11:14:52 +0900

    Update custom_text_form_field.dart

--- 03.플러터PJ/app_04_login/lib/components/custom_text_form_field.dart
+++ 03.플러터PJ/app_04_login/lib/components/custom_text_form_field.dart
@@ -22,7 +22,34 @@ class CustomTextFormField extends StatelessWidget {
           // 2-1.유효성 검사 셋팅 (출력 메시지)
           // !(느낌표)는 절때 null이 아님을 의미한다. (컴파일러에 알림)
           // 비었으면 메시지 / null은 메시지 없음
-          validator: (value) => value!.isEmpty? "Please enter some text" : null 
+          validator: (value) => value!.isEmpty? "Please enter some text" : null,
+          // 2-2. 비밀번호인 경우 글자 *** 처리하기
+          obscureText: text == 'Password' ? true : false,
+          // 2-3. 입력폼 구성하기
+          decoration: InputDecoration(
+            // 2-3-1. 입력내용 힌트 텍스트
+            hintText: 'Enter $text',
+            // 2-3-2. 기본 TextFormField 디자인
+            enabledBorder: OutlineInputBorder(
+            // 테두리 디자인 설정하기
+              borderRadius: BorderRadius.circular(20)
+            ),
+            // 2-3-3. 터치시 (포커스 갈때) TextFormField 디자인
+            focusedBorder:  OutlineInputBorder(
+              // 테두리 디자인 설정하기
+              borderRadius: BorderRadius.circular(20),
+            ),
+            // 2-3-4. 에러발생시 TextFormField 디자인
+            errorBorder:  OutlineInputBorder(
+              // 테두리 디자인 설정하기
+              borderRadius: BorderRadius.circular(20),
+            ),
+            // 2-3-5. 에러발생후 터치시 TextFormField 디자인
+            focusedErrorBorder: OutlineInputBorder(
+              // 테두리 디자인 설정하기
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
         ),
       ],
     );
